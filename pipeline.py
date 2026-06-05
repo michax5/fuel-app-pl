85120e89ae7e1c5a42281cdbe4f0288df89a668d
michax5 <michal.lewandowski@inowroclaw.zhr.pl>
2026-06-05 17:00:41 +0200

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -4,7 +4,9 @@ import yfinance as yf
 import requests
 
 # ========= ROPA =========
+
 oil = yf.download("BZ=F", period="3mo")
+oil.columns = oil.columns.get_level_values(0)
 df_oil = oil[["Close"]].rename(columns={"Close": "price_brent"})
 df_oil = df_oil.reset_index()
 df_oil["date"] = pd.to_datetime(df_oil["Date"])
